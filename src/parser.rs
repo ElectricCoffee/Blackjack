951ba769858595d2f731dd58a34527fa49710a3b
Niko Lepka <slench102+git@gmail.com>
2018-10-01 17:06:16 +0200

    update hitting and bidding to accommodate the hand change

--- src/parser.rs
+++ src/parser.rs
@@ -15,22 +15,26 @@ lazy_static! {
 /// Parses a regex match and returns a command
 /// 
 /// Turns a match into a string, trims it, then parses it
-fn parse_match<T, F>(mat: regex::Match, f: F) -> Option<Command> 
-where T: FromStr, F: FnOnce(T) -> Command {
-    mat.as_str().trim().parse::<T>().ok().map(f)
+fn parse_match<T: FromStr>(mat: regex::Match) -> Option<T> {
+    mat.as_str().trim().parse::<T>().ok()
 }
 
 /// Parses a string using regular expressions, then throws back a Command
 pub fn parse_input(input: &str) -> Option<Command> {
     if BID.is_match(input) {
         BID.captures(input)
-            .and_then(|cap| cap.get(1))
-            .and_then(|mat| parse_match(mat, Bid))
+            .and_then(|cap| {
+                let bid = cap.get(1).and_then(parse_match); // get 1st capture, and parse it to a u64
+                let idx = cap.get(2).and_then(parse_match).or(Some(1)); // get 2nd capture, and parse it to a usize
+
+                bid.and_then(|b| idx.map(|i| Bid(b, i))) // wrap it in a Bid
+            })
     } else if HIT.is_match(input) {
         HIT.captures(input)
-            .and_then(|cap| cap.get(1))
-            .and_then(|mat| parse_match(mat, Hit))
-            .or(Some(Hit(1)))
+            .and_then(|cap| cap.get(1)) // get 1st capture
+            .and_then(parse_match) // parse to a usize
+            .map(Hit) // wrap it in a Hit
+            .or(Some(Hit(1))) // if None, return Hit(1)
     } else if SPLIT.is_match(input) {
         Some(Split)
     } else if QUIT.is_match(input) {
