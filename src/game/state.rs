73b8e6c10b07e87589466451ea8b1b6fc5de11dd
Niko Lepka <slench102+git@gmail.com>
2018-10-03 14:54:19 +0200

    update state struct
    now holds a vec of winnings and the current bid is a vec of bids

--- src/game/state.rs
+++ src/game/state.rs
@@ -1,22 +1,25 @@
 use cards::*;
 use rand::ThreadRng;
+use game::flow::Winning;
 
 pub struct State {
     pub deck: Deck,
     pub player_hands: Vec<Vec<Card>>,
+    pub player_wins: Vec<Winning>,
     pub dealer_hand: Vec<Card>,
+    pub current_bids: Vec<u64>,
     pub earnings: u64,
-    pub current_bid: u64,
 }
 
 impl State {
     pub fn new(money: u64, rng: &mut ThreadRng) -> State {
         State { 
             deck: Deck::new().shuffled(rng),
-            player_hands: Vec::new(),
-            dealer_hand: Vec::new(),
+            player_hands: vec![],
+            player_wins:  vec![Winning::Playing],
+            dealer_hand:  vec![],
+            current_bids: vec![0],
             earnings: money, 
-            current_bid: 0,
         }
     }
 }
